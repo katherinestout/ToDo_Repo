71b090d8b855145400ff911bab40d8f9083629c2
katie stout <katiestout@katies-MBP.home>
2018-08-14 16:59:44 -0400

    hard way

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,6 +4,32 @@
 
 $(document).ready(function(){
     $("li").click(function(){
-        alert("connected");
+       // alert("connected");
+       
+       //$(this).css("color", "gray");
+       //$(this).css("text-decoration", "line-through");
+
+        //if li is gray, turn it black
+        //else turn it gray
+
+        if($(this).css("color") === "rgb(128, 128, 128)"){
+           // console.log("its gray!")}
+
+
+            $(this).css({
+                color:"black",
+                textDecoration: "none"
+            });
+        }
+        
+    else {
+
+        $(this).css({
+            color:"gray",
+            textDecoration: "line-through"
+        });
+
+    }
+     
     });
 });
